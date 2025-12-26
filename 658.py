6647e080cfcb4f017532755a722c3fdb519d26a7
Mironov Boris <lboris201@yandex.ru>
2025-12-26 20:55:02 +0300

    Finished Leetcode 658:

--- 658.py
+++ 658.py
@@ -1,8 +1,13 @@
 from typing import List
 
+# вариает с поиском ближайшего числа через бинарный поиск и двумя поинтерами ищем бижайшие числа
 class Solution:
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        if len(arr) == k:
+            return arr
+        
         def find_idx_x(arr, x):
+            # binary search
             l, r = 0, len(arr) - 1
             while l <= r:
                 mid = (l + r) // 2
@@ -10,20 +15,49 @@ class Solution:
                     return mid
                 if arr[mid] < x:
                     l = mid + 1
-                else:
+                if arr[mid] > x:
                     r = mid - 1
+            
+            if l == len(arr):
+                l -= 1
+
+            # closest value
+            if 0 < l < len(arr) and abs(x - arr[l - 1]) <= abs(x - arr[l]):
+                l -= 1
+            elif 0 <= l < len(arr) - 1 and abs(x - arr[l + 1]) < abs(x - arr[l]):
+                l += 1
             return l
         
         idx = find_idx_x(arr, x)
-        radius = k // 2
-        dr = 0
-        
-        if len(arr) > idx + radius:
-            dr = len(arr)
 
-arr = [1,2,3,4,5]
-k = 4
-x = 4
+        l, r = idx, idx
+        k -= 1
+        while k > 0:
+            if (r == len(arr) - 1) or (l > 0 and r < len(arr) - 1 and (abs(x - arr[l - 1]) <= abs(x - arr[r + 1]))):
+                l -= 1
+                k -= 1
+            elif (r < len(arr) - 1):
+                r += 1
+                k -=1
 
+        return arr[l:r + 1] 
+
+arr = [0,0,1,2,3,3,4,7,7,8]
+k = 3
+x = 5
 solution = Solution()
 print(solution.findClosestElements(arr, k, x))
+
+
+# и нормальный вариант где мы делаем lower bound бинарный поиск где ищем срез (ответ) проверяя крайние точки среза на близость к нужному числу
+# типо если левая граница ближе чем правая, то движемся влево
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        l, r = 0, len(arr) - k
+        while l < r:
+            mid = (l + r) // 2
+            if x - arr[mid] > arr[mid + k] - x:
+                l = mid + 1
+            else:
+                r = mid
+        return arr[l:l + k]
