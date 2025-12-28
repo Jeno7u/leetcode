2ca2f7410189f81f1039c79db239d2dfb07ec70d
Mironov Boris <lboris201@yandex.ru>
2025-12-28 21:59:31 +0300

    Leetcode 125

---
+++ solutions/125.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"unicode"
+)
+
+func isPalindrome(s string) bool {
+	l, r := 0, len(s) - 1
+	for l < r {
+		if !unicode.IsLetter(rune(s[l])) && !unicode.IsNumber(rune(s[l])) {
+			l++
+			continue
+		}
+		if !unicode.IsLetter(rune(s[r])) && !unicode.IsNumber(rune(s[r])){
+			r--
+			continue
+		}
+		if unicode.ToLower(rune(s[l])) != unicode.ToLower(rune(s[r])) {
+			return false
+		}
+		l++
+		r--
+	}
+	return true
+}
+
+func main() {
+	s := "race a car"
+	fmt.Println(isPalindrome(s))
+}
