1d918c6aace7d472bca426ba38ed6fe8af2ef197
Mironov Boris <lboris201@yandex.ru>
2026-01-09 00:16:23 +0300

    Leetcode 283

---
+++ solutions/283.go
@@ -0,0 +1,17 @@
+package main
+
+// проходимся двумя поинтерами. j указывает на место где все числа до него не нулевые, то есть это место вставки
+func moveZeroes(nums []int)  {
+    j := 0 // индекс куда вставлять значение не нулевое
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			nums[i], nums[j] = nums[j], nums[i]
+			j++ 
+		}
+	}
+}
+
+func main() {
+	nums := []int{0,1,0,3,12}
+	moveZeroes(nums)
+}
