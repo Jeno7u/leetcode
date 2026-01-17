8321fabcad5cc803a92c261065a9f8d27644ca1c
Mironov Boris <lboris201@yandex.ru>
2026-01-17 14:05:12 +0300

    Leetcode 567

---
+++ solutions/567.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+// проверяем крайний случай, потом вместо hash map можем исполоьзовать просто [26]int. И вместо вычесления количества букв для каждого sliding window
+// мы просто вычитаем первое значение и прибовляем последнее + 1 (это эффективней)
+func checkInclusion(s1 string, s2 string) bool {
+    if len(s2) < len(s1) {
+        return false
+    }
+
+    charAmount1 := make([]int, 26)
+    charAmount2 := make([]int, 26)
+    for i := range s1 {
+        charAmount1[int(s1[i]) - int('a')]++
+        charAmount2[int(s2[i]) - int('a')]++
+    }
+
+    if slices.Equal(charAmount1, charAmount2) {
+        return true
+    }
+
+    for i := 0; i < len(s2) - len(s1); i++ {
+        charAmount2[int(s2[i]) - int('a')]--
+        charAmount2[int(s2[i + len(s1)]) - int('a')]++
+		if slices.Equal(charAmount1, charAmount2) {
+			return true
+		}
+    }
+
+	return false
+}
+
+
+func main() {
+	s1 := "ab"
+	s2 := "eidbaooo"
+	fmt.Println(checkInclusion(s1, s2))
+}
