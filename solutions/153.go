b444e963947b38bceea1a9dd402bc549c7343a0e
Mironov Boris <lboris201@yandex.ru>
2026-01-20 00:49:40 +0300

    Leetcode 153

---
+++ solutions/153.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+// по сути является половиной задачи Leetcode 33. Мы просто делаем lower bound binary search с условием <= nums[len(nums) - 1]
+func findMin(nums []int) int {
+    l, r := 0, len(nums)
+	for l < r {
+		mid := (l + r) / 2
+		if nums[mid] <= nums[len(nums) - 1] {
+			r = mid
+		} else {
+			l = mid + 1
+		}
+	}
+	return nums[l]
+}
+
+func main() {
+	nums := []int{4}
+	fmt.Println(findMin(nums))
+}
