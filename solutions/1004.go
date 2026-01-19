8f202d76156ad7d6eb538f785c400203fd46a150
Mironov Boris <lboris201@yandex.ru>
2026-01-19 22:57:49 +0300

    Leetcode 1004

---
+++ solutions/1004.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+// движемся вправо через r до момоента пока zerosCount <= k. Если больше, то мы двигаем левый поинтер пока nums[l] не будет равен 0, постоянно сдвигая l
+// ну и result у нас это просто расстояние между l и r
+func longestOnes(nums []int, k int) int {
+    l, zerosCount, result := 0, 0, 0
+    for r := 0; r < len(nums); r++ {
+        if nums[r] == 0 {
+            zerosCount++
+        }
+        for zerosCount > k {
+            if nums[l] == 0 {
+                zerosCount--
+            }
+            l++
+        }
+        result = max(result, r - l + 1)
+    }
+    return result
+}
+
+func main() {
+	nums := []int{0,1,1}
+	k := 0
+	fmt.Println(longestOnes(nums, k))
+}
